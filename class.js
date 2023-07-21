23cc3d36da38e30cb2f270512af1c08912cbfb16
Keith Kariru Mutuku <karirukeith@students.uonbi.ac.ke>
2023-07-21 21:47:43 +0300

    practice inheritance an encapsulation in demo class

--- class.js
+++ class.js
@@ -1,10 +1,25 @@
 "use strict";
+//Employee class
 class Employee {
     constructor(id, name, address) {
-        this.address = address;
         this.id = id;
         this.name = name;
+        this.address = address;
+    }
+    getNameWithAddress() {
+        return `${this.name} stays at ${this.address}`;
     }
 }
 let john = new Employee(1, "John", "Nyayo Estate");
 console.log(john);
+let address = john.getNameWithAddress();
+console.log(address);
+//Inheritance
+class SalesEmployee extends Employee {
+    constructor(id, name, address, department) {
+        super(id, name, address);
+        this.department = department;
+    }
+}
+let junior = new SalesEmployee(25, "Junior", "Bellevue", "Medical");
+console.log(junior);
--- class.ts
+++ class.ts
@@ -1,15 +1,39 @@
+//Employee class
 class Employee {
-    id: number;
+    protected id: number;
     name: string;
     address: string
 
     constructor(id: number, name: string, address: string) {
-        this.address = address;
         this.id = id;
         this.name = name
+        this.address = address;
+    }
+
+    getNameWithAddress(): string {
+        return `${this.name} stays at ${this.address}`;
     }
 }
 
 let john = new Employee(1, "John", "Nyayo Estate");
 
-console.log(john)
+console.log(john);
+
+let address = john.getNameWithAddress();
+
+console.log(address);
+
+//Inheritance
+class SalesEmployee extends Employee {
+    private department: string;
+
+    constructor(id: number, name: string, address: string, department: string) {
+        super(id, name, address);
+        this.department = department
+    }
+}
+
+let junior = new SalesEmployee(25, "Junior", "Bellevue", "Medical")
+
+console.log(junior)
+
