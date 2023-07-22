2038fa5f235c62855ab21f3eaafcfcecd4de8393
Keith Kariru Mutuku <karirukeith@students.uonbi.ac.ke>
2023-07-22 23:33:29 +0300

    practice inheritance in interfaces

--- interface.js
+++ interface.js
@@ -1,1 +1,16 @@
 "use strict";
+let user = {
+    name: "Keith",
+    age: 15,
+    id: 140102,
+    email: "karirukeith@gmail.com"
+};
+console.log(user);
+let james = {
+    name: "Keith",
+    age: 15,
+    id: 140102,
+    email: "karirukeith@gmail.com",
+    salary: 500
+};
+console.log(james);
--- interface.ts
+++ interface.ts
@@ -1,6 +1,31 @@
+//example of interface
 interface User {
     name: string;
     age: number;
     id: number;
     email: string;
 }
+
+let user: User = {
+    name: "Keith",
+    age: 15,
+    id: 140102,
+    email: "karirukeith@gmail.com"
+}
+
+console.log(user)
+
+//inheritance using interfaces
+interface Manager extends User {
+    salary: number;
+}
+
+let james: Manager = {
+    name: "Keith",
+    age: 15,
+    id: 140102,
+    email: "karirukeith@gmail.com",
+    salary: 500
+}
+
+console.log(james)
